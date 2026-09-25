5c50ea739ad4788c77466c56b117223b3c6ab12e
lukaszpeciak91-sys <lukaszpeciak91@gmail.com>
2026-09-25 19:25:10 +0200

    Use coroutines for lookup UI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,6 +44,7 @@ dependencies {
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.kotlinx.coroutines.android)
     implementation(libs.okhttp)
     implementation(libs.kotlinx.serialization.json)
 
