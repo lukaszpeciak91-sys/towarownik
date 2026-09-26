57654bcd078e3dfc5facfc59f16acc88d7d2eec0
lukaszpeciak91-sys <lukaszpeciak91@gmail.com>
2026-09-26 12:37:14 +0200

    Bump app version to 0.1.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "pl.lukaszpeciak.towarownik"
         minSdk = 26
         targetSdk = 36
-        versionCode = 3
-        versionName = "0.1.2"
+        versionCode = 4
+        versionName = "0.1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
